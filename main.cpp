f6891aba7c5d863dabe8c064e053165b377ba4d0
Eugene M <darkstarmacho@gmail.com>
2021-07-26 10:23:20 +0300

    Implement colur histogram

--- main.cpp
+++ main.cpp
@@ -11,26 +11,45 @@ static const int HEIGHT = 600;
 int main() {
     Bitmap bmp(WIDTH, HEIGHT);
 
-    double min = 999999;
-    double max = -999999;
+    std::unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS]{0});
+    std::unique_ptr<int[]> fractal(new int[WIDTH * HEIGHT] {0});
 
     for(int y = 0; y < HEIGHT; ++y) {
         for(int x = 0; x < WIDTH; ++x) {
-            double xFractal = (x - WIDTH/2.0 - 200) * 2.0/WIDTH;
+            double xFractal = (x - WIDTH/2.0 - 200) * 2.0/HEIGHT;
             double yFractal = (y - HEIGHT/2.0) * 2.0/HEIGHT;
 
             int iterations = Mandelbrot::getIterations(xFractal, yFractal);
 
-            uint8_t red = (uint8_t)(256 * (double)iterations/Mandelbrot::MAX_ITERATIONS);
+            fractal[y * WIDTH + x] = iterations;
 
-            bmp.setPixel(x, y, red, 0x0, 0x0);
-
-            if(red < min) min = red;
-            if(red > max) max = red;
+            if(iterations != Mandelbrot::MAX_ITERATIONS)
+                histogram[iterations]++;
         }
     }
 
-    std::cout << min << ", " << max << std::endl;
+    int total = 0;
+    for(int i = 0; i < Mandelbrot::MAX_ITERATIONS; ++i) {
+        total += histogram[i];
+    }
+
+     for(int y = 0; y < HEIGHT; ++y) {
+        for(int x = 0; x < WIDTH; ++x) {
+            int iterations = fractal[y * WIDTH + x];
+
+            double hue = 0.0;
+            for(int i = 0; i <= iterations; ++i) {
+                hue += ((double)histogram[i]) / total;
+            }
+
+            uint8_t red = 0;
+            uint8_t green = hue * 255;
+            uint8_t blue = 0;
+
+            bmp.setPixel(x, y, red, green, blue);
+
+        }
+     }
 
     bmp.write("test.bmp");
 
